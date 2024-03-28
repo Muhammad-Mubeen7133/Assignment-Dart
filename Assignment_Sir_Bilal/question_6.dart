19e746054b0d3756c150b983839db1621103d3a8
Muhammad-Mubeen7133 <m.muneen7133@gmail.com>
2024-03-29 02:30:30 +0500

    Logic

---
+++ Assignment_Sir_Bilal/question_6.dart
@@ -0,0 +1,35 @@
+void main() {
+  Map world = {
+    'countries': {
+      'Pakistan': {
+        'capitalCity': 'Islamabad',
+        'currency': 'rupee',
+        "language": "Urdu"
+      },
+      'Afghanistan': {
+        'capitalCity': 'Qabul',
+        'currency': 'Afghan Afghani',
+        "language": "Pushtoo and Dari",
+      },
+      "France": {
+        "capitalCity": "Paris",
+        "currency": "Euro",
+        "language": "French",
+      },
+    },
+  };
+  final France = world['countries']['Afghanistan'];
+
+  print('Capital city of is: ${France['capitalCity']}');
+  print('Currency is : ${France['currency']}');
+  print('Language is : ${France['language']}');
+}
+
+// Q.6: Create Map variable name world then inside it create countries Map, 
+// Key will be the name country & country value will have another map having capitalCity,
+//  currency and language to it. by using any country key print all the value of Capital & Currency.
+
+
+
+
+
